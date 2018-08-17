6da21ffd9008d6ad295ed7c39e2ffbbf7c1c8b0e
Scaldabagno <federico.augello@community.unipa.it>
2018-08-17 11:35:30 +0200

    Bugfix

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
 function hash(){
 
     console.log($('#cryptoSelect').val());
-    if($('#cryptoSelect').val() == "MD5"){
+    if($('#cryptoSelect').val() === "MD5"){
         $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.MD5(input);
             document.getElementById('output').value = hash;
             return true;
-        });
-        }if($('#cryptoSelect').val() == "SHA-1"){
+            });
+        }if($('#cryptoSelect').val() === "SHA-1"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA1(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-256"){
+        }if($('#cryptoSelect').val() === "SHA-256"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA256(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-384"){
+        }if($('#cryptoSelect').val() === "SHA-384"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA384(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-512"){
+        }if($('#cryptoSelect').val() === "SHA-512"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA512(input);
@@ -40,35 +40,35 @@ function hash(){
 
     $('#cryptoSelect').on('click',function() {
         console.log($('#cryptoSelect').val());
-        if($('#cryptoSelect').val() == "MD5"){
+        if($('#cryptoSelect').val() === "MD5"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.MD5(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-1"){
+        }if($('#cryptoSelect').val() === "SHA-1"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA1(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-256"){
+        }if($('#cryptoSelect').val() === "SHA-256"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA256(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-384"){
+        }if($('#cryptoSelect').val() === "SHA-384"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA384(input);
             document.getElementById('output').value = hash;
             return true;
             });
-        }if($('#cryptoSelect').val() == "SHA-512"){
+        }if($('#cryptoSelect').val() === "SHA-512"){
             $('#hash').click(function(event){
             var input = document.getElementById('input').value;
             var hash = CryptoJS.SHA512(input);
@@ -76,7 +76,16 @@ function hash(){
             return true;
             });
         }
-  });
+    });
+
+    $('#reset').on('click',function() {
+        $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.MD5(input);
+            document.getElementById('output').value = hash;
+            return true;
+        });
+    });
 }
 
 
