2c8cd5a1b20b4c00bb33bb089deb57f001ec95d5
Scaldabagno <federico.augello@community.unipa.it>
2018-08-17 02:20:04 +0200

    Added everything

---
+++ js/index.js
@@ -0,0 +1,83 @@
+function hash(){
+
+    console.log($('#cryptoSelect').val());
+    if($('#cryptoSelect').val() == "MD5"){
+        $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.MD5(input);
+            document.getElementById('output').value = hash;
+            return true;
+        });
+        }if($('#cryptoSelect').val() == "SHA-1"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA1(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-256"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA256(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-384"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA384(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-512"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA512(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }
+
+    $('#cryptoSelect').on('click',function() {
+        console.log($('#cryptoSelect').val());
+        if($('#cryptoSelect').val() == "MD5"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.MD5(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-1"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA1(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-256"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA256(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-384"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA384(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }if($('#cryptoSelect').val() == "SHA-512"){
+            $('#hash').click(function(event){
+            var input = document.getElementById('input').value;
+            var hash = CryptoJS.SHA512(input);
+            document.getElementById('output').value = hash;
+            return true;
+            });
+        }
+  });
+}
+
+
+$(document).ready(hash);
