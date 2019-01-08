dd94acf01aa2c0006749b921fc9909585ad10c97
Scaldabagno <federico.augello@community.unipa.it>
2019-01-08 16:10:46 +0100

    Switch instead of if

--- js/index.js
+++ js/index.js
@@ -8,42 +8,46 @@ copyHash1.addEventListener('click', copyHashFunction, false)
 
 function hashFunction(){
     // Verifies Dropdown value
-    if(document.getElementById('cryptoSelect').value === "MD5"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.MD5(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }if(document.getElementById('cryptoSelect').value === "SHA-1"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.SHA1(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }if(document.getElementById('cryptoSelect').value === "SHA-224"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.SHA224(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }if(document.getElementById('cryptoSelect').value === "SHA-256"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.SHA256(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }if(document.getElementById('cryptoSelect').value === "SHA-384"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.SHA384(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }if(document.getElementById('cryptoSelect').value === "SHA-512"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.SHA512(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }if(document.getElementById('cryptoSelect').value === "SHA-3"){
-            var input = document.getElementById('input').value;
-            var hash = CryptoJS.SHA3(input);
-            document.getElementById('output').value = hash;
-            return true;
-    }
+    var choice = document.getElementById('cryptoSelect').value;
+    switch(choice){
+        case "MD5":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.MD5(input);
+                document.getElementById('output').value = hash;
+                break;
+        case "SHA-1":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.SHA1(input);
+                document.getElementById('output').value = hash;
+                break;
+        case "SHA-224":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.SHA224(input);
+                document.getElementById('output').value = hash;
+                break;
+        case "SHA-256":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.SHA256(input);
+                document.getElementById('output').value = hash;
+                break;
+        case "SHA-384":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.SHA384(input);
+                document.getElementById('output').value = hash;
+                break;
+        case "SHA-512":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.SHA512(input);
+                document.getElementById('output').value = hash;
+                break;
+        case "SHA-3":
+                var input = document.getElementById('input').value;
+                var hash = CryptoJS.SHA3(input);
+                document.getElementById('output').value = hash;
+                break;
+        default:
+                console.log("No Encryption selected");
+        }
 }
 
 function copyHashFunction(){
